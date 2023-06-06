d3ef7f967d8b6b8b19281b8dea87aa855009248c
Lorys89 <64613690+Lorys89@users.noreply.github.com>
2023-06-06 18:11:30 +0200

    Added preliminary macOS 14 support (#75)

--- VirtualSMC/kern_start.cpp
+++ VirtualSMC/kern_start.cpp
@@ -33,7 +33,7 @@ PluginConfiguration ADDPR(config) {
 	bootargBeta,
 	arrsize(bootargBeta),
 	KernelVersion::MountainLion,
-	KernelVersion::Ventura,
+	KernelVersion::Sonoma,
 	[]() {
 		auto prov = VirtualSMCProvider::getInstance();
 		if (prov)
